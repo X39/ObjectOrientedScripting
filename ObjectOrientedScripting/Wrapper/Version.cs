6bb9af9a3a0716974c367b7761d59de1064f51dc
Marco.Silipo <m.silipo@web.de>
2015-03-24 19:28:59 +0100

    Added Constructor, fixed Warnings

--- ObjectOrientedScripting/Wrapper/Version.cs
+++ ObjectOrientedScripting/Wrapper/Version.cs
@@ -14,6 +14,24 @@ namespace ObjectOrientedScripting
         public int Main { get { return this._main; } set { this._main = value; } }
         public int Side { get { return this._side; } set { this._side = value; } }
         public int Revision { get { return this._rev; } set { this._rev = value; } }
+        public Version(string s)
+        {
+            string[] sArr = s.Split('.');
+            switch(sArr.Length)
+            {
+                default:
+                    throw new ArgumentException("Unknown Version Syntax");
+                case 3:
+                    this.Revision = Convert.ToInt32(sArr[2]);
+                    goto case 2;
+                case 2:
+                    this.Side = Convert.ToInt32(sArr[1]);
+                    goto case 1;
+                case 1:
+                    this.Main = Convert.ToInt32(sArr[0]);
+                    break;
+            }
+        }
         public override string ToString()
         {
             return _main + "." + _side + "." + _rev;
@@ -57,6 +75,14 @@ namespace ObjectOrientedScripting
         public static bool operator !=(Version x, Version y)
         {
             return x._main != y._main && x._side != y._side && x._rev != y._rev;
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
