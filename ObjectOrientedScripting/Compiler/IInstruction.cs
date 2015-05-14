88438871dd6c26bece19383fc726e9b28fc82b78
Marco.Silipo <m.silipo@web.de>
2015-05-14 03:00:38 +0200

    Changed comments to IntelliSense function comments

--- ObjectOrientedScripting/Compiler/IInstruction.cs
+++ ObjectOrientedScripting/Compiler/IInstruction.cs
@@ -6,21 +6,53 @@ using System.Threading.Tasks;
 
 namespace Compiler.OOS_LanguageObjects
 {
-    public interface IInstruction
+    public interface IInstruction : IFinalizable
     {
-        /**Prints out given instruction into StreamWriter as SQF. writer object is either a string or a StreamWriter*/
+        /// <summary>
+        /// Prints out given instruction into StreamWriter as SQF. writer object is either a string or a StreamWriter
+        /// </summary>
+        /// <param name="writer">Contains either a string or a StreamWriter depending on the object</param>
+        /// <param name="printTabs">Sets wether this IInstruction should print instructions</param>
         void printInstructions(object writer, bool printTabs = true);
-        /**Parses given string input specially for this element (example use: foreach(var foo in bar) would replace every occurance of foo with _x and every occurence of _x with __x or something like that)*/
+        /// <summary>
+        /// Parses given string input specially for this element
+        /// Should refer to parents parseInput function if not used
+        /// 
+        /// example use:
+        /// foreach(var foo in bar) would replace every occurance of foo with _x
+        /// and every occurence of _x with __x or something like that
+        /// </summary>
+        /// <param name="input">A variable name</param>
+        /// <returns>The new variable name</returns>
         string parseInput(string input);
-        /**returns parent IInstruction which owns this IInstruction (only will return null for the oos namespace object which is the root node for anything)*/
+        /// <summary>
+        /// returns parent IInstruction which owns this IInstruction (only will return null for the oos namespace object which is the root node for everything)
+        /// </summary>
+        /// <returns>Objects parent IInstruction</returns>
         IInstruction getParent();
-        /**returns a list of child IInstructions with given type*/
+        /// <summary>
+        /// Iterates current IInstruction tree up/down and returns given type
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="recursiveUp"></param>
+        /// <param name="recursiveDown"></param>
+        /// <returns>Array of child IInstructions with given type</returns>
         IInstruction[] getInstructions(Type t, bool recursiveUp = true, bool recursiveDown = false);
-        /**returns first occurance of given type in tree or NULL if nothing was found*/
+        /// <summary>
+        /// Searches for the first occurance in current IInstruction tree of given type
+        /// </summary>
+        /// <param name="t">type to search for</param>
+        /// <returns>Either the first object hit while moving the tree upward or null</returns>
         IInstruction getFirstOf(Type t);
-        /**Adds given instruction to child instruction list and checks if it is valid to own this instruction*/
+        /// <summary>
+        /// Adds given instruction to child instruction list and checks if it is valid to own this instruction
+        /// </summary>
+        /// <param name="instr">IInstruction to add</param>
         void addInstruction(IInstruction instr);
-        /**returns current tab ammount*/
+        /// <summary>
+        /// "Calculate" current tab ammount
+        /// </summary>
+        /// <returns>Ammount of tab for current scope layer</returns>
         int getTabs();
     }
 }
