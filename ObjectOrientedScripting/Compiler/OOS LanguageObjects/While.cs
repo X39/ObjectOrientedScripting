eb4f3774a2e7cec7f7847e8ab16f70e4215c5067
Marco.Silipo <m.silipo@web.de>
2015-05-14 03:29:32 +0200

    Started implementing While

--- ObjectOrientedScripting/Compiler/OOS LanguageObjects/While.cs
+++ ObjectOrientedScripting/Compiler/OOS LanguageObjects/While.cs
@@ -9,16 +9,67 @@ namespace Compiler.OOS_LanguageObjects
 {
     class While : IInstruction
     {
+        private IInstruction _parent;
+        private Expression _condition;
+        private IInstruction _loopContent;
+        private While(IInstruction parent)
+        {
+            this._parent = parent;
+        }
         //IFinalizable
         public void finalize() { throw new NotImplementedException(); }
         //IInstruction
         public static While parse(StreamReader reader, IInstruction parent, string currentLine) { throw new NotImplementedException(); }
-        public void printInstructions(object writer, bool printTabs = true) { throw new NotImplementedException(); }
-        public string parseInput(string input) { throw new NotImplementedException(); }
-        public IInstruction getParent() { throw new NotImplementedException(); }
-        public IInstruction[] getInstructions(Type t, bool recursiveUp = true, bool recursiveDown = false) { throw new NotImplementedException(); }
-        public IInstruction getFirstOf(Type t) { throw new NotImplementedException(); }
-        public void addInstruction(IInstruction instr) { throw new NotImplementedException(); }
-        public int getTabs() { throw new NotImplementedException(); }
+        public void printInstructions(object writer, bool printTabs = true)
+        {
+            if (!(writer is System.IO.StreamWriter))
+                throw new Exception("printInstruction expected a StreamWriter object but received a " + writer.GetType().Name + " object");
+            ((System.IO.StreamWriter)writer).Write("\r\n" + (printTabs ? new string('\t', this.getTabs()) : "") + "while {");
+            this._condition.printInstructions(writer, false);
+            ((System.IO.StreamWriter)writer).Write("} do");
+            ((System.IO.StreamWriter)writer).Write("\r\n" + (printTabs ? new string('\t', this.getTabs()) : "") + "{");
+            if (typeof(Scope).IsInstanceOfType(this._loopContent))
+                ((Scope)this._loopContent).printInstructions(writer, printTabs, false, null);
+            else
+                this._loopContent.printInstructions(writer, printTabs);
+            ((System.IO.StreamWriter)writer).Write("\r\n" + (printTabs ? new string('\t', this.getTabs()) : "") + "};");
+        }
+        public string parseInput(string input)
+        {
+            return this._parent.parseInput(input);
+        }
+        public IInstruction getParent()
+        {
+            return this._parent;
+        }
+        public IInstruction[] getInstructions(Type t, bool recursiveUp = true, bool recursiveDown = false)
+        {
+            List<IInstruction> result = new List<IInstruction>();
+            if (recursiveUp && recursiveDown)
+                throw new Exception("Cannot move up AND down at the same time");
+            if (t.IsInstanceOfType(this))
+                result.Add(this);
+            if (recursiveUp)
+                this._parent.getInstructions(t, recursiveUp, recursiveDown);
+            if (recursiveDown)
+            {
+                result.AddRange(this._condition.getInstructions(t, recursiveUp, recursiveDown));
+                result.AddRange(this._loopContent.getInstructions(t, recursiveUp, recursiveDown));
+            }
+            return result.ToArray();
+        }
+        public IInstruction getFirstOf(Type t)
+        {
+            IInstruction firstOccurance = this.getParent().getFirstOf(t);
+            return (firstOccurance == null ? (t.IsInstanceOfType(this) ? this : null) : firstOccurance);
+        }
+        public void addInstruction(IInstruction instr)
+        {
+            throw new Exception("An Identifier cannot have sub instructions");
+        }
+        public int getTabs()
+        {
+            return this._parent.getTabs();
+        }
     }
 }
