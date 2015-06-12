33d65056217de10666f8b9e3fb1edc2eee394992
Marco.Silipo <m.silipo@web.de>
2015-06-12 15:10:18 +0200

    Started implementing GlobalFunction #12

--- ObjectOrientedScripting/Compiler/OOS LanguageObjects/GlobalFunction.cs
+++ ObjectOrientedScripting/Compiler/OOS LanguageObjects/GlobalFunction.cs
@@ -9,10 +9,47 @@ namespace Compiler.OOS_LanguageObjects
 {
     class GlobalFunction : IFunction
     {
+        private Scope _scope;
+        private string _functionName;
+        private IInstruction _parent;
+
+        private GlobalFunction(IInstruction parent, string functionName)
+        {
+            this._parent = parent;
+            this._scope = null;
+            this._functionName = functionName;
+        }
         //IFinalizable
         public void finalize() { throw new NotImplementedException(); }
         //IInstruction
-        public static GlobalFunction parse(StreamReader reader, string currentLine) { throw new NotImplementedException(); }
+        public static GlobalFunction parse(IInstruction parent, StreamReader reader, string currentLine)
+        {
+            if (!currentLine.StartsWith("static ", StringComparison.OrdinalIgnoreCase))
+                throw new Exception("Global function declaration without static keyword is not allowed. currentLine: " + currentLine);
+            currentLine = currentLine.Substring("static".Length).Trim();
+            if (!currentLine.StartsWith("function ", StringComparison.OrdinalIgnoreCase))
+                throw new Exception("Declaration without purpose detected (example: 'static function foobar() {}'). currentLine: " + currentLine);
+            currentLine = currentLine.Substring("function".Length).Trim();
+            string functionName = currentLine.Substring(0, currentLine.IndexOf('('));
+            currentLine = currentLine.Substring(currentLine.IndexOf('('));
+            if (!System.Text.RegularExpressions.Regex.IsMatch(functionName, @"^[_a-zA-Z][_a-zA-Z0-9]+$"))
+                throw new Exception("Function name '" + functionName + @"' is not allowed (REGEX match failed '^[_a-zA-Z][_a-zA-Z0-9]+$'). currentLine: " + currentLine);
+            List<IInstruction> arguments = new List<IInstruction>();
+            GlobalFunction globalFunction = new GlobalFunction(parent, functionName);
+            while(true)
+            {
+                string s = currentLine.Substring(0, currentLine.IndexOfAny(new char[] { ',', ')' }));
+                currentLine = currentLine.Substring(s.Length);
+                if (s.Length == 0)
+                    break;
+                if (!System.Text.RegularExpressions.Regex.IsMatch(functionName, @"^[_a-zA-Z][_a-zA-Z0-9]+$"))
+                    throw new Exception("Variable name '" + s + @"' is not allowed (REGEX match failed '^[_a-zA-Z][_a-zA-Z0-9]+$'). currentLine: " + currentLine);
+                arguments.Add(LocalVariable.parse(globalFunction, "var _" + s + ";"));
+            }
+            Scope scope = Scope.parse(globalFunction, reader, arguments);
+            globalFunction._scope = scope;
+            return globalFunction;
+        }
         public void printInstructions(object writer, bool printTabs = true) { throw new NotImplementedException(); }
         public string parseInput(string input) { throw new NotImplementedException(); }
         public IInstruction getParent() { throw new NotImplementedException(); }
