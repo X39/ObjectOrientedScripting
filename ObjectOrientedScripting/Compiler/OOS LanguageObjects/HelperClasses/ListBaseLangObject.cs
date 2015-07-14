952c64a9d2e3c48c11d17df567194efeb982f4cd
Marco.Silipo <m.silipo@web.de>
2015-07-14 20:24:25 +0200

    Fixed a ton of syntax issues

---
+++ ObjectOrientedScripting/Compiler/OOS LanguageObjects/HelperClasses/ListBaseLangObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.OOS_LanguageObjects.HelperClasses
+{
+    public class ListBaseLangObject
+    {
+        List<BaseLangObject> list;
+
+        public void add(BaseLangObject o)
+        {
+            list.Add(o);
+        }
+        public List<BaseLangObject> getList()
+        {
+            return list;
+        }
+    }
+}
---
+++ ObjectOrientedScripting/Compiler/OOS LanguageObjects/HelperClasses/ListString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.OOS_LanguageObjects.HelperClasses
+{
+    public class ListString
+    {
+        List<string> list;
+
+        public void add(string o)
+        {
+            list.Add(o);
+        }
+        public List<string> getList()
+        {
+            return list;
+        }
+    }
+}
