715335686e6f6f82664170452497814b8f8faf51
Marco.Silipo <m.silipo@web.de>
2015-11-22 19:56:58 +0100

    introduced enums

---
+++ ObjectOrientedScripting/Compiler/OOS/LanguageObjects/oosEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.OOS_LanguageObjects
+{
+    public class oosEnum : pBaseLangObject, Interfaces.iName
+    {
+        public Ident Name { get { return ((Ident)this.children[0]); } set { this.children[0] = value; } }
+        public oosEnum(pBaseLangObject parent) : base(parent)
+        {
+            this.children.Add(null);
+        }
+        public override int doFinalize()
+        {
+            int errCount = 0;
+            return errCount;
+        }
+        public override string ToString()
+        {
+            return "enum->" + this.Name.FullyQualifiedName;
+        }
+        public override void writeOut(System.IO.StreamWriter sw, SqfConfigObjects.SqfConfigFile cfg)
+        {
+
+        }
+    }
+}
