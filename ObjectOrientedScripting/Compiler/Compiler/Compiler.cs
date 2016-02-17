8604fdb0f2e9d9688dc0dd0092ad19446901dda2
Marco.Silipo <m.silipo@web.de>
2016-02-17 20:15:55 +0100

    0.7.3-ALPHA release preparations

--- ObjectOrientedScripting/Compiler/Compiler/Compiler.cs
+++ ObjectOrientedScripting/Compiler/Compiler/Compiler.cs
@@ -119,7 +119,7 @@ namespace Wrapper
         }
         public Version getVersion()
         {
-            return new Version("0.7.2-ALPHA");
+            return new Version("0.7.3-ALPHA");
         }
         public void CheckSyntax(string filepath)
         {
